f145320c64b52ad46f74630873ce1a6d8dbd023c
Hafid <hafid.saou@gmail.com>
2023-05-21 23:22:41 +0200

    add registerProposal and addVote functions

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -36,9 +36,12 @@ contract Voting is Ownable {
 
     WorkflowStatus workflowStatus;
     mapping(address => Voter) voters;
+    mapping(uint => Proposal) proposals;
+    uint proposalId;
 
     constructor() {
         workflowStatus = WorkflowStatus.RegisteringVoters;
+        proposalId = 0;
     }
 
     function getWinner() external returns (uint) {}
@@ -57,18 +60,42 @@ contract Voting is Ownable {
         onlyOwner
         emitWorkflowChange
     {
+        require(
+            workflowStatus == WorkflowStatus.RegisteringVoters,
+            "Current workflow should be RegisteringVoters"
+        );
         workflowStatus = WorkflowStatus.ProposalsRegistrationStarted;
     }
 
     function endProposalsRegistration() external onlyOwner emitWorkflowChange {
+        require(
+            workflowStatus == WorkflowStatus.ProposalsRegistrationStarted,
+            "Current workflow should be ProposalsRegistrationStarted"
+        );
         workflowStatus = WorkflowStatus.ProposalsRegistrationEnded;
     }
 
     function startVotingSession() external onlyOwner emitWorkflowChange {
+        require(
+            workflowStatus == WorkflowStatus.ProposalsRegistrationEnded,
+            "Current workflow should be ProposalsRegistrationEnded"
+        );
         workflowStatus = WorkflowStatus.VotingSessionStarted;
     }
 
+    function endVotingSession() external onlyOwner emitWorkflowChange {
+        require(
+            workflowStatus == WorkflowStatus.VotingSessionStarted,
+            "Current workflow should be VotingSessionStarted"
+        );
+        workflowStatus = WorkflowStatus.VotingSessionEnded;
+    }
+
     function tallyVote() external onlyOwner emitWorkflowChange {
+        require(
+            workflowStatus == WorkflowStatus.VotingSessionEnded,
+            "Current workflow should be VotingSessionEnded"
+        );
         workflowStatus = WorkflowStatus.VotesTallied;
     }
 
@@ -76,11 +103,48 @@ contract Voting is Ownable {
         Voting functions
     */
     function addVoter(address _voter) external onlyOwner {
+        require(
+            workflowStatus == WorkflowStatus.RegisteringVoters,
+            "Not in a registering voters session"
+        );
         Voter memory voter;
         voter.isRegistered = false;
         voter.hasVoted = false;
         voter.votedProposalId = 0;
         voters[_voter] = voter;
         emit VoterRegistered(_voter);
+    }
+
+    function registerProposal(string memory _description) external {
+        require(
+            workflowStatus == WorkflowStatus.ProposalsRegistrationStarted,
+            "Not in a proposal register session"
+        );
+        require(voters[msg.sender].isRegistered, "Voter not registred");
+        require(
+            voters[msg.sender].votedProposalId != 0,
+            "Voter already proposed"
+        );
+
+        proposalId++;
+        Proposal memory proposal;
+        proposal.description = _description;
+        proposal.voteCount = 0;
+        proposals[proposalId] = proposal;
+
+        voters[msg.sender].votedProposalId = proposalId;
+        emit ProposalRegistered(proposalId);
+    }
+
+    function addVote(uint _proposalId) external {
+        require(
+            workflowStatus == WorkflowStatus.VotingSessionStarted,
+            "Not in a vote session"
+        );
+        require(voters[msg.sender].isRegistered, "Voter not registred");
+        require(_proposalId <= proposalId, "proposalId not valid");
+
+        proposals[_proposalId].voteCount += 1;
+        emit Voted(msg.sender, _proposalId);
     }
 }
